1acc45f09fcb62eea832085b73700d7c619f0ea3
MiloValenzuela <40413618+MiloValenzuela@users.noreply.github.com>
2023-07-09 22:44:17 -0600

    variables practice and some operators

--- variables.go
+++ variables.go
@@ -3,6 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Milo"
+	fmt.Println("my name is", myName, myName)
+
+	var name string = "Judith"
+	fmt.Println("name =", name)
+
 	var favColor = "Red and Blue"
 	fmt.Println("My favorite color is", favColor )
+
+	var sum int
+	fmt.Println("The sum is", sum)
+
+	part1, other := 1, 5
+	fmt.Println("part 1 is", part1, "other is", other)
 }
+
